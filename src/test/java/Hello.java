e86b4ba0905407ef986a37c4458bbfc9ebfa5179
lidiya <lidiyamuna@gmail.com>
2021-05-22 11:33:32 -0500

    work 1 and 2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         //enhancing hello
+        //work 1
+        //work 2
 
     }
 }
